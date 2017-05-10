7d8aefe7e7f9a4c8fc1be3be33edf897a0c0f7c5
LiMeng Liu <v-limliu@microsoft.com>
2017-05-10 11:48:50 +0800

    change version to 1.6.0.1

--- DeviceAdministration/Web/Constants.cs
+++ DeviceAdministration/Web/Constants.cs
@@ -6,7 +6,7 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web
 {
     public static class Constants
     {
-        public const string JSVersion = "1.6.0.0";
+        public const string JSVersion = "1.6.0.1";
         public const string CultureCookieName = "_culture";
         public const string DeviceIconTagName = "__icon__";
         public const string DeviceUserTagName = "__UserName__";
